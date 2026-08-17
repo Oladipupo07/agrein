9d94cae1d289f94b783429e78272109ca42b320b
Oladipupo Akobe <akobeoladipupo@gmail.com>
2026-08-17 16:40:07 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Agrein Progressive Web App Service Worker
 // Provides offline functionality, caching strategy, and background sync
 
-const CACHE_NAME = 'agrein-v2';
+const CACHE_NAME = 'agrein-v3';
 const urlsToCache = [
   '/',
   '/index.html',
